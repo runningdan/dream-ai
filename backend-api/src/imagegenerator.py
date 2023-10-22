d4236a243f35d4d6b2208e8bb6a838992723b90d
Dan White <danwhite9143@gmail.com>
2023-10-21 20:17:15 -0400

    connected server to image generation class

--- backend-api/src/imagegenerator.py
+++ backend-api/src/imagegenerator.py
@@ -1,18 +1,19 @@
 import http.client
 import json
+import time
+from loguru import logger
+
+
+class ImageGenerationException(Exception):
+    pass
 
 class ImageGeneratorAI:
-
-
     def __init__(self, imagine_api_key):
         # constructor
-
         self.imagine_api_key = imagine_api_key
-        
-
 
     def generate_prompt(self, product, attributes):
-        # convert the desired product and an array of attributes 
+        # convert the desired product and an array of attributes
         # into a single string prompt that can be read and interpreted
 
         prompt = product + " with the following attributes: "
@@ -25,26 +26,24 @@ class ImageGeneratorAI:
                 prompt += element + ", "
         return prompt
 
-
-
     def generate_image_urls(self, prompt):
-        data = {
-            "prompt": prompt
-        }
-    
+        data = {"prompt": prompt}
+
         headers = {
-            'Authorization': 'Bearer ' + self.imagine_api_key
-            'Content-Type': 'application/json'
+            "Authorization": "Bearer " + self.imagine_api_key,
+            "Content-Type": "application/json",
         }
-    
+
         conn = http.client.HTTPSConnection("demo.imagineapi.dev")
         conn.request("POST", "/items/images/", body=json.dumps(data), headers=headers)
-    
+
         response = conn.getresponse()
-        response_data = json.loads(response.read().decode('utf-8'))
-    
+        response_data = json.loads(response.read().decode("utf-8"))
+
+        logger.info("response data: " + response_data)
+
         image_id = response_data["data"]["id"]
- 
+
         max_time_out = 60
         iterations = 0
 
@@ -55,10 +54,37 @@ class ImageGeneratorAI:
                 response = connection.getresponse()
                 get_images_response = json.loads(response.read().decode())
 
-                if get_images_response["data"]["status"] == "complete":
+                logger.info("get image response data: " + get_images_response)
+
+                if get_images_response["data"]["status"] == "completed":
                     return get_images_response["data"]["upscaled_urls"]
+
+                if get_images_response["data"]["status"] == "failed":
+                    raise ImageGenerationException(
+                        "status failed when attempting to fetch image"
+                    )
 
             except Exception as error:
                 print(f"Error: {error}")
 
             iterations += 1
+
+            time.sleep(5)
+
+        raise TimeoutError("timeout exceeded when fetching images")
+
+class ImageGeneratorAIDryRun(ImageGeneratorAI):
+    def generate_image_urls(self, prompt):
+        logger.debug("DRY RUN: prompt: " + prompt)
+        return [
+            "https://demo.imagineapi.dev/assets/073ba657-664b-4c0a-9a39-3eeae6252e2c/073ba657-664b-4c0a-9a39-3eeae6252e2c.png",
+            "https://demo.imagineapi.dev/assets/416b55e0-8bb0-4010-8c4d-ffe710e85793/416b55e0-8bb0-4010-8c4d-ffe710e85793.png",
+            "https://demo.imagineapi.dev/assets/09ec4563-0a79-437f-89f7-fe82e999475c/09ec4563-0a79-437f-89f7-fe82e999475c.png",
+            "https://demo.imagineapi.dev/assets/0e3840fe-5c5f-4dd8-bf38-f34f36e7f4f6/0e3840fe-5c5f-4dd8-bf38-f34f36e7f4f6.png"
+        ]
+
+
+def create_image_generator(dry_run, api_key):
+    if dry_run == "true":
+        return ImageGeneratorAIDryRun(api_key)
+    return ImageGeneratorAI(api_key)
--- backend-api/src/server.py
+++ backend-api/src/server.py
@@ -3,23 +3,35 @@ from pydantic import BaseModel
 from typing import List
 from dotenv import load_dotenv
 import os.path
+import imagegenerator
 
-if os.path.exists(".env.dev"):
-    load_dotenv(".env.dev")
+
+if os.path.exists("../.env.dev"):
+    load_dotenv("../.env.dev")
+
+# todo fix types
+image_generator: imagegenerator.ImageGeneratorAI = imagegenerator.create_image_generator(
+    os.getenv("DRY_RUN"),
+    os.getenv("IMAGINE_API_KEY")
+)
 
 class GenerateImageRequest(BaseModel):
     attributes: List[str]
     product: str
 
+
 class GenerateImageResponse(BaseModel):
     upscaled_images: List[str]
 
+
 app = FastAPI()
 
+
 @app.post("/api/v1/generate-image")
-async def generate_image(req: GenerateImageRequest, response_model=GenerateImageResponse):
-    return GenerateImageResponse(
-        upscaled_images=[
-            "https://demo.imagineapi.dev/assets/00cc56e8-1797-4886-b268-d6bf2d89d583/00cc56e8-1797-4886-b268-d6bf2d89d583.png"
-        ]
-    )
+async def generate_image(
+    req: GenerateImageRequest, response_model=GenerateImageResponse
+):
+    prompt: str = image_generator.generate_prompt(req.product, req.attributes)
+    upscaled_image_links: List[str] = image_generator.generate_image_urls(prompt)
+
+    return GenerateImageResponse(upscaled_images=upscaled_image_links)
